d84e6ecd7bcb808f663fd4d449e58c9947e73ac8
Сметанка <derivolkate152@gmail.com>
2021-10-09 22:12:39 +0300

    added having a nice day wish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 int main() {
-  std::cout << "Hello world!\n";
+    char name[20];
+  cout << "Enter your name: ";
+  cin >> name;
+  cout << "Hello, " << name << "!\n";
+  cout << "Nice to see you learning Git\n";
+  cout << "Wish you to have a nice day\n";
   system("pause");
   return 0;
 }
